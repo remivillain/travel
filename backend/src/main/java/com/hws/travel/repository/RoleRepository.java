98fd46b8ce56bc6666b0653d7e0c54268c5a105a
Rémi VILLAIN <remi.v.10@hotmail.fr>
2025-09-09 18:59:24 +0200

    feat : add role repo

---
+++ backend/src/main/java/com/hws/travel/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.hws.travel.repository;
+
+import com.hws.travel.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
