15489f37d9498475502461881cfc3de681d181d9
Rémi VILLAIN <remi.v.10@hotmail.fr>
2025-09-10 17:15:34 +0200

    fix : back allowed origins config

--- backend/src/main/java/com/hws/travel/config/WebConfig.java
+++ backend/src/main/java/com/hws/travel/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200", "http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
